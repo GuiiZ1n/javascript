6b2fe005271d3b8edb83d1c9689c9f5c5a1d5ef5
GuiiZ1n <guigui_araujo@hotmail.com>
2025-04-22 19:33:33 +0100

    JavaScript
    Primeiros conceitos de javascript.

--- projeto/index.js
+++ projeto/index.js
@@ -1,1 +1,30 @@
-console.log('Olá Mundo!');
+var olaMundo = "Hello World";
+
+
+//Aqui eu escrevo no console
+console.log(olaMundo);
+console.log(olaMundo);
+console.log(olaMundo);
+//Aqui eu crio um alerta
+alert(olaMundo);
+
+let a = 10;
+const b = "10";
+/* = atribuição
+   == Comparação de valor
+   === Comparação de valor e igualdade
+   !== Diferente
+   != Compara valor
+   || ou
+*/
+console.log(a != b && typeof a == 'string');
+
+let cor = "verde";
+if (cor === "verde") {
+
+    console.log("siga")
+
+} else {
+    
+    console.log("pare")
+};
